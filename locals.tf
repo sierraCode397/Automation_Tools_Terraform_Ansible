c8118e8aff42a952bd40b519d27c138d58a1b960
izaack107 <izaack107@gmail.com>
2025-03-30 21:12:27 -0600

    module composition, (vpc)

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  name   = "complete-mysql"
+  region = "us-east-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/terraform-aws-modules/terraform-aws-rds"
+  }
+}
--- main.tf
+++ main.tf
@@ -12,19 +12,7 @@ terraform {
 
 data "aws_availability_zones" "available" {}
 
-locals {
-  name   = "complete-mysql"
-  region = "us-east-1"
 
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  tags = {
-    Name       = local.name
-    Example    = local.name
-    Repository = "https://github.com/terraform-aws-modules/terraform-aws-rds"
-  }
-}
 
 ################################################################################
 # Access keys
