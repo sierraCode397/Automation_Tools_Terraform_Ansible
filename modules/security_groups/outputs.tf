f0099b0814ca6824d0a48a8f2482775100a62aa7
izaack107 <izaack107@gmail.com>
2025-04-01 20:35:04 -0600

    module composition, (Implement security-groups using locals instead)

--- modules/security_groups/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "security_group_id" {
-  description = "The ID of the created security group"
-  value       = module.security_group.this_security_group_id
-}
-
-output "security_group_ids" {
-  description = "A list of security group IDs"
-  value       = module.security_group.this_security_group_ids
-}
